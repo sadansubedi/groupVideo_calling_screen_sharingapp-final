c5df273eff62eb6437bc1a666e16807ee8b8b5e6
sadansubedi <82010812+sadansubedi@users.noreply.github.com>
2023-10-10 19:28:27 +0545

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -6,8 +6,8 @@ import {
 } from "agora-rtc-react";
 
 
- const appid ="d1c732e22f2743d7b97184d404a6536e";
- const token ="007eJxTYAiZ4PnYXWVNkTTfHsaK7H2n95zVeJG1/NcO81+solc6U30UGFIMk82NjVKNjNKMzE2MU8yTLM0NLUxSTAxMEs1Mjc1Sj3xWSW0IZGRI0DzDxMgAgSA+D0NxclFqal5xRmJRqhEDAwA+IiIk"
+ const appid ="get it from agora https://console.agora.io/projects";
+ const token ="same get it from agora ok "
 
 export const config = { mode: "rtc", codec: "vp8", appID: appid, token: token };
 export const useClient = createClient(config);
